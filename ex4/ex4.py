a10ab6bf9cf2c173a4a405c582f3fe9ff73176da
Jessica <jessica.battle@gmail.com>
2015-08-02 15:13:50 -0700

    update to exercise 4

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -15,3 +15,16 @@ cars_not_driven = cars - drivers
 
 #cars someone is driving
 cars_driven = drivers
+
+#total number of people that can be transported in carpool
+carpool_capacity = cars_driven * space_in_a_car
+
+#average number of passengers in each car
+average_passengers_per_car = passengers / cars_driven
+
+print "There are", cars , "cars available."
+print "There are only", drivers , "drivers available."
+print "There will be", cars_not_driven , "empty cars today."
+print "We can transport", carpool_capacity , "people today."
+print "We have", passengers , "who want to carpool today."
+print "We need to put about", average_passengers_per_car , "people in each car."

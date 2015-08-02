3b65bbb472e2a997c6f95e6a2616e933cfa6747f
Jessica <jessica.battle@gmail.com>
2015-08-02 14:41:40 -0700

    initial commit

---
+++ ex4/ex4.py
@@ -0,0 +1,17 @@
+#100 cars
+cars = 100
+
+#4.0 units of space in a cars
+space_in_a_car = 4.0
+
+#30 drivers
+drivers = 30
+
+#90 passengers
+passengers = 90
+
+#cars no one is driving
+cars_not_driven = cars - drivers
+
+#cars someone is driving
+cars_driven = drivers

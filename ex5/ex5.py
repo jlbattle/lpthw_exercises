41c44a2377841198342779a3b2f74fd474696d98
Jessica <jessica.battle@gmail.com>
2015-08-05 06:25:57 -0700

    add exercise 5

---
+++ ex5/ex5.py
@@ -0,0 +1,41 @@
+my_name = 'Muzzy the Alien'
+my_age = 130			#not a lie, I'm an alien
+my_height = 100		#inches
+my_weight = 400 	#lbs
+my_eyes = 'Red'
+my_teeth = 'Yellow'
+my_hair = 'Blue'
+
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "That's pretty heavy!"
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+#this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+
+#Study Drill 1
+name = "Muzzy the Alien"
+age = 130			#no lie, I'm an alien!
+height = 100	#inches
+weight = 400	#lbs
+eyes = "Red"
+teeth = "Yellow"
+hair = "Blue"
+
+print "===Study Drill 1==="
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % (teeth)
+print "If I add %d, %d, and %d I get %d." %(age, height, weight, age + height + weight)
+
+#Study Drill 2 - convert inches to centimeters and lbs to kilograms
+
+#Study Drill 3 - More format characters
+
